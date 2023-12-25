41fa189cdc41d3cb2726967e852aec1b0afbb82a
sorryINoe <128090280+sorryINoe@users.noreply.github.com>
2023-12-26 01:48:28 +0300

    Add files via upload

--- Main.java
+++ Main.java
@@ -1,19 +1,18 @@
 import java.util.Scanner;
 
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-        int a,b,c;
-        System.out.printf("Введите числа b и c для вычесления выражения a=4*(b+c-1)/2" + "\n"
-                + "Введите b" + "\n");
-        b = scanner.nextInt();
-        System.out.printf("Введите c" + "\n");
-        c = scanner.nextInt();
-        a=0;
-        a = 4*(b+c-1)/2;
+        int n,a,b;
 
-        System.out.println( "a=" + a );
+        System.out.printf("Введите двухзанчное число" + "\n");
+        n = scanner.nextInt();
+        a = n/10%100;
+        b = n%10;
+        n = a+b;
+
+        System.out.println("Сумма цифр в числе "+n);
+
+
     }
 }

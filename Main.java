9e41e401003a72e7d4457942e6a1a24c97dd8bb9
sorryINoe <128090280+sorryINoe@users.noreply.github.com>
2023-12-26 01:46:43 +0300

    Add files via upload

--- Main.java
+++ Main.java
@@ -1,16 +1,19 @@
 import java.util.Scanner;
 
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
-
-
-
-        System.out.printf("Enter user name" + "\n") ;
-
         Scanner scanner = new Scanner(System.in);
-        String username = scanner.nextLine();
+        int a,b,c;
+        System.out.printf("Введите числа b и c для вычесления выражения a=4*(b+c-1)/2" + "\n"
+                + "Введите b" + "\n");
+        b = scanner.nextInt();
+        System.out.printf("Введите c" + "\n");
+        c = scanner.nextInt();
+        a=0;
+        a = 4*(b+c-1)/2;
 
-        System.out.printf("Hello " + username);
-
+        System.out.println( "a=" + a );
     }
 }

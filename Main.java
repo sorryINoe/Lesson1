625a1a7c6bc41118fe7632090f608643d72c285f
sorryINoe <128090280+sorryINoe@users.noreply.github.com>
2023-12-26 01:49:42 +0300

    Add files via upload

--- Main.java
+++ Main.java
@@ -3,13 +3,14 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-        int rez1,rez2,q,w;
-        System.out.printf("Введите 2 натуральных числа" + "\n");
-        q = scanner.nextInt();
-        w = scanner.nextInt();
+        int a,b;
+        System.out.printf("Введите 2 числа" + "\n");
+        a = scanner.nextInt();
+        b=a ;
+        a = scanner.nextInt();
 
-        rez1 =  q / w;
-        rez2 = q % w;
-        System.out.println("Результат деления " + rez1 + " " + "остаток " + rez2);
+
+        System.out.println("Замена мест " + a + " "+ b);
+
     }
 }

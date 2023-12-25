4c5117933e59a281e8cae6f828cbe351bba5ea63
sorryINoe <128090280+sorryINoe@users.noreply.github.com>
2023-12-26 01:48:46 +0300

    Add files via upload

--- Main.java
+++ Main.java
@@ -3,16 +3,16 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-        int n,a,b;
-
-        System.out.printf("Введите двухзанчное число" + "\n");
+        int n,a,b,c;
+        System.out.printf("Введите трехзначное число" + "\n");
         n = scanner.nextInt();
-        a = n/10%100;
-        b = n%10;
-        n = a+b;
 
-        System.out.println("Сумма цифр в числе "+n);
+        a = n / 100 % 10;
+        b = n / 10 % 10;
+        c = n % 10;
+        n = a+b+c;
 
+        System.out.println("Cумма цифр в числе " + n);
 
     }
 }
